85a9bc85bea21d367c85dae3bca7f8d0274cfcba
stevenlustig <steven@androidsonskateboards.com>
2015-06-21 18:20:36 -0400

    Moved AOSUtilsBase into subdirectory to be combined into "AOSUtils" repo

---
+++ AOSUtilsBase/src/org/aosutils/AOSConstants.java
@@ -0,0 +1,7 @@
+package org.aosutils;
+
+public class AOSConstants {
+	//public final static String USER_AGENT_DESKTOP = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)";
+	public final static String USER_AGENT_DESKTOP = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0";
+	public final static String CHARACTER_ENCODING = "UTF-8";
+}
