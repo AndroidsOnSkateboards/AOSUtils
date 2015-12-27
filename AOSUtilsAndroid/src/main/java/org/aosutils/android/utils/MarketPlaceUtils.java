74aeeaeaefcf5cec010859dacdb641a1af962142
stevenlustig <steven@androidsonskateboards.com>
2015-12-27 02:55:13 -0500

    Added MarketPlaceUtils

---
+++ AOSUtilsAndroid/src/main/java/org/aosutils/android/utils/MarketPlaceUtils.java
@@ -0,0 +1,35 @@
+package org.aosutils.android.utils;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+
+public class MarketPlaceUtils {
+	public enum MarketPlace { GooglePlay, Amazon, Other }
+	
+	public static String getMarketPlace(Context context) {
+		String installerId = null;
+		
+		try {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+				installerId = getInstallerId(context);
+			}
+		}
+		catch (Exception e) { }
+		
+		if ("com.android.vending".equals(installerId) || "com.google.android.feedback".equals(installerId)) {
+			return MarketPlace.GooglePlay.toString();
+		}
+		else if ("com.amazon.venezia".equals(installerId)) {
+		    return MarketPlace.Amazon.toString(); 
+		}
+		else {
+			return installerId;
+		}
+	}
+	
+	@TargetApi(Build.VERSION_CODES.ECLAIR)
+	private static String getInstallerId(Context context) {
+		return context.getPackageManager().getInstallerPackageName(context.getPackageName());
+	}
+}
